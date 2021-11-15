3adf3695c8fc4318a50e9c456cb0e501bac42918
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-15 14:48:36 +0530

    MazePath problem solution

--- src/recursion/MazePath.java
+++ src/recursion/MazePath.java
@@ -1,4 +1,17 @@
 package recursion;
 
 public class MazePath {
+    public static void main(String[] args) {
+        mazePath(0,0,3,4,"");
+    }
+    public static void mazePath(int currentRow, int currentColumn, int endRow, int endColumn, String ans){
+        if(currentRow==endRow-1 && currentColumn==endColumn-1){
+            System.out.println(ans);
+        }
+        if(currentRow>=endRow || currentColumn>=endColumn){
+            return;
+        }
+        mazePath(currentRow, currentColumn+1, endRow,endColumn, ans+"H");
+        mazePath(currentRow+1, currentColumn, endRow,endColumn, ans+"V");
+    }
 }
