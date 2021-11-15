700e8fceb9d6075e4e1331130b35a69f3d84bfd1
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-15 14:58:35 +0530

    MazePath problem solution

--- src/recursion/MazePath.java
+++ src/recursion/MazePath.java
@@ -10,8 +10,9 @@ public class MazePath {
     public static void mazePath(int currentRow, int currentColumn, int endRow, int endColumn, String ans){
         if(currentRow==endRow-1 && currentColumn==endColumn-1){
             System.out.println(ans);
+            return;
         }
-        if(currentRow>=endRow || currentColumn>=endColumn){
+        if(currentRow >= endRow || currentColumn >= endColumn){
             return;
         }
         mazePath(currentRow, currentColumn+1, endRow,endColumn, ans+"H");
