f5d118ba02256e526c779bcdfd987c39f0a1ef1f
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-15 14:52:08 +0530

    MazePath problem solution

--- src/recursion/MazePath.java
+++ src/recursion/MazePath.java
@@ -4,7 +4,9 @@ package recursion;
 //program to print all possible paths from starting cell of a matrix to ending cell
 public class MazePath {
     public static void main(String[] args) {
-        mazePath(0,0,3,4,"");
+        int row = 3;
+        int column = 3;
+        mazePath(0,0,row,column,"");
     }
     public static void mazePath(int currentRow, int currentColumn, int endRow, int endColumn, String ans){
         if(currentRow==endRow-1 && currentColumn==endColumn-1){
