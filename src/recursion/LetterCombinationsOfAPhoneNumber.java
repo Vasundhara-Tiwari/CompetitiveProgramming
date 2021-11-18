f84a6922518e737a5007f8591ef8b7b9ee5c4a1c
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-18 20:48:58 +0530

    Letter Combinations Of A Phone Number problem solution

--- src/recursion/LetterCombinationsOfAPhoneNumber.java
+++ src/recursion/LetterCombinationsOfAPhoneNumber.java
@@ -1,5 +1,20 @@
 package recursion;
 
 public class LetterCombinationsOfAPhoneNumber {
-
+    static String[] getKey = {"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wx", "yz"};
+    public static void main(String[] args) {
+        String str = "198";
+        keyPad(str, "");
+    }
+    public static void keyPad(String ques, String ans){
+        if(ques.length()==0){
+            System.out.println(ans+" ");
+            return;
+        }
+        char ch = ques.charAt(0);
+        String key = getKey[ch-48];
+        for(int i=0; i<key.length(); i++){
+            keyPad(ques.substring(1), ans+key.charAt(i));
+        }
+    }
 }
