8d49b0753fa0232533bb34f81c056c2a40c68b3e
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-22 13:53:53 +0530

    creating a new method printAnswer

--- src/recursion/Permutation.java
+++ src/recursion/Permutation.java
@@ -1,4 +1,10 @@
 package recursion;
 
 public class Permutation {
+    public static void main(String[] args) {
+
+    }
+    public static void printAnswer(String question, String answer){
+
+    }
 }
