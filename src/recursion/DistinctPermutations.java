bbf66edb43755c6fe233cd895a96280a54494032
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-22 14:58:56 +0530

    implementing method distinctPermutations

--- src/recursion/DistinctPermutations.java
+++ src/recursion/DistinctPermutations.java
@@ -2,9 +2,17 @@ package recursion;
 
 public class DistinctPermutations {
     public static void main(String[] args) {
-
+        String str = "ABCA";
+        distinctPermutations(str, "");
     }
-    public static void distinctPermutations(){
+    public static void distinctPermutations(String ques, String ans){
 
+        for(int i=0; i<ques.length(); i++){
+            char ch = ques.charAt(i);
+            boolean flag = false;
+            for(int j=i+1; j<ques.length(); j++){
+
+            }
+        }
     }
 }
