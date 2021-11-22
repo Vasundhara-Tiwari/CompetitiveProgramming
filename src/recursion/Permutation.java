4cdf5c5360a9758d15212ce39444af23f5aa73f0
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-22 14:17:52 +0530

    printing permutations using recursion

--- src/recursion/Permutation.java
+++ src/recursion/Permutation.java
@@ -2,9 +2,19 @@ package recursion;
 
 public class Permutation {
     public static void main(String[] args) {
-
+        String ques = "ABC";
+        String ans = "";
+        printAnswer(ques, ans);
     }
     public static void printAnswer(String question, String answer){
-
+        if(question.length() == 0){
+            System.out.println(answer);
+            return;
+        }
+        for(int i=0; i<question.length(); i++) {
+            char ch = question.charAt(i);
+            String part = question.substring(0, i) + question.substring(i+1);
+            printAnswer(part,answer+ch);
+        }
     }
 }
